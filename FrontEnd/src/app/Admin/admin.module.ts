b5c5578915c07a2be65f88d4ff3bfdd407e2ff9f
Shaikh Mustafa <66483021+Shaikh-Mustafa@users.noreply.github.com>
2022-12-18 22:15:24 +0530

    Added Admin Site

--- FrontEnd/src/app/Admin/admin.module.ts
+++ FrontEnd/src/app/Admin/admin.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { NavigationComponent } from './navigation/navigation.component';
 const adminRouter:Routes=[
-  // {path:'',component:AdminComponent}
+  {path:'',component:AdminComponent}
 ]
 
 @NgModule({
   declarations: [
 
+  
+    NavigationComponent
   ],
   imports: [
     CommonModule,
