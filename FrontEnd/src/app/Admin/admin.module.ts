46436421c15b13374ae420b26cbc5f95680d0eaf
Shaikh Mustafa <mustafas@eksource.com>
2023-01-17 14:35:25 +0530

    Added Admin Site Components

--- FrontEnd/src/app/Admin/admin.module.ts
+++ FrontEnd/src/app/Admin/admin.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './admin/Components/dashboard/dashboard.component';
+import { ProductsComponent } from './admin/Components/products/products.component';
+import { SalesComponent } from './admin/Components/sales/sales.component';
+import { CustomersComponent } from './admin/Components/customers/customers.component';
+import { ComplaintsComponent } from './admin/Components/complaints/complaints.component';
+import { FAQComponent } from './admin/Components/faq/faq.component';
+import { SettingsComponent } from './admin/Components/settings/settings.component';
 const adminRouter:Routes=[
   {path:'',component:AdminComponent}
 ]
 
 @NgModule({
   declarations: [  
-    NavigationComponent
+    NavigationComponent, DashboardComponent, ProductsComponent, SalesComponent, CustomersComponent, ComplaintsComponent, FAQComponent, SettingsComponent
   ],
   imports: [
     CommonModule,
